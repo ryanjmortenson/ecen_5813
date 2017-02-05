a1cc4b03e0f812bb39d0df281967b1adcc49ab55
rmortens <ryan.j.mortenson@gmail.com>
2017-02-05 10:56:28 -0700

    Change set_3 to be ASCII numbers

--- project/app/src/project1.c
+++ project/app/src/project1.c
@@ -6,10 +6,11 @@
 #include "project1.h"
 
 #define ARRAY_SIZE 32
+#define MAX_ASCII_INT_CHARS 4
 
 uint8_t set_1[ARRAY_SIZE];
 uint8_t set_2[ARRAY_SIZE];
-uint8_t set_3[ARRAY_SIZE];
+uint8_t set_3[ARRAY_SIZE][MAX_ASCII_INT_CHARS];
 
 void project_1_report()
 {
@@ -39,19 +40,6 @@ void project_1_report()
     counter++;
   }
 
-  // Create an array of characters abcdefghijklmnopqrstuvwxyzABCDEF
-  counter = 0;
-  for(int k = 'a'; k <= 'z'; k++)
-  {
-    *(set_3 + k) = k;
-    counter++;
-  }
-
-  for(int k = 'A'; k <= 'F'; k++)
-  {
-    *(set_3 + k) = k;
-    counter++;
-  }
 } // project_1_report()
 
 void test_data1()
@@ -73,24 +61,24 @@ void test_data1()
 
 void test_data2()
 {
-  uint8_t buffer[100];
-  my_memset(buffer, 0, 100);
+  // Make a buffer to store returns from my_atoi()
+  uint32_t buffer[ARRAY_SIZE];
+  my_memset((uint8_t *)buffer, ARRAY_SIZE*sizeof(uint32_t)/sizeof(uint8_t), 0);
 
   // Convert all integers in buffer to ASCII characters
   printf("Converting integers into ASCII characters\n");
   for(int i = 0; i < ARRAY_SIZE; i++)
   {
-    my_itoa((int8_t *)buffer, *(set_2 + i), 10);
-    print_memory(buffer, 10);
-    printf("%s \n", buffer);
+    my_itoa((int8_t *)*(set_3 + i), *(set_2 + i), 10);
+    print_memory(*(set_3 + i), MAX_ASCII_INT_CHARS);
   }
 
   // Convert all ASCII characters in buffer to integers
   printf("Converting ASCII characters into integers\n");
   for(int i = 0; i < ARRAY_SIZE; i++)
   {
-    *(buffer + i) = my_atoi((int8_t *)&*(set_2 + i));
-    print_memory((uint8_t *)buffer, 4);
+    *(buffer + i) = my_atoi((int8_t *)*(set_3 + i));
+    print_memory((uint8_t *)(buffer + i), 4);
   }
 } // test_data2()
 
@@ -106,7 +94,6 @@ void test_memory()
   my_memmove((set_2 + 25), (set_2 + 20), 6);
   my_memzero((set_2 + 11), 3);
   my_memmove(set_2, (set_2 + 8), 8);
-
 
   // Show memory after
   printf("Memory after manipulation\n");
