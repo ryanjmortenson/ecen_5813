1a0c81b8af1d441fe09285772c0a9edd050b3e62
rmortens <ryan.j.mortenson@gmail.com>
2017-02-05 09:48:04 -0700

    Add compile time flag for project1

--- project/makeall.sh
+++ project/makeall.sh
@@ -1,3 +1,3 @@
-make out/kl25z/main 
-make out/linux-arm/main 
-make out/linux-x86/main 
+make out/kl25z/main FLAG=PROJECT1
+make out/linux-arm/main FLAG=PROJECT1
+make out/linux-x86/main FLAG=PROJECT1
