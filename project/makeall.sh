d70003011d7319e89b2e8c7502af5db07d31dd41
rmortens <ryan.j.mortenson@gmail.com>
2017-02-05 10:57:12 -0700

    Remove flag from makeall.sh

--- project/makeall.sh
+++ project/makeall.sh
@@ -1,3 +1,4 @@
-make out/kl25z/main FLAG=PROJECT1
-make out/linux-arm/main FLAG=PROJECT1
-make out/linux-x86/main FLAG=PROJECT1
+make clean
+make out/kl25z/main
+make out/linux-arm/main
+make out/linux-x86/main
