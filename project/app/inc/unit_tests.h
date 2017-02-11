1523334de0d3331d3cb8ca2087d012ae2cb33618
rmortens <ryan.j.mortenson@gmail.com>
2017-02-11 15:32:06 -0700

    Add unit tests

---
+++ project/app/inc/unit_tests.h
@@ -0,0 +1,17 @@
+#ifndef __UNIT_TESTS_H__
+#define __UNIT_TESTS_H__
+
+
+/*
+ * \brief memory_unit: used to execute memory.c unit tests
+ *
+ */
+void memory_unit();
+
+/*
+ * \brief data_unit: used to execute data.c unit tests
+ *
+ */
+void data_unit();
+
+#endif
