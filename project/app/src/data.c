1277b680ac2fe3768d9de1014e946a42faa4c3e7
rmortens <ryan.j.mortenson@gmail.com>
2017-02-18 17:24:32 -0700

    Fix consistency and bug:
    - Most for loops used int instead of uint32_t
    - Fix bug with bases being < 2 and 36

--- project/app/src/data.c
+++ project/app/src/data.c
@@ -29,7 +29,7 @@ int8_t * my_itoa(int8_t * str, int32_t data, int32_t base)
   }
 
   // Base is incorrect return pointer to str
-  if(base > 35)
+  if(base > 36 || base < 2)
   {
     return start;
   }
@@ -113,7 +113,7 @@ int8_t big_to_little32(uint32_t * data, uint32_t length)
   CHECK_NULL(data);
 
   // Loop over data swapping bytes
-  for(int i = 0; i < length; i++)
+  for(uint32_t i = 0; i < length; i++)
   {
     uint32_t value = *(data + i);
     *(data + i) = ((value & 0xff000000) >> 24 |
@@ -140,10 +140,10 @@ void print_memory(uint8_t * start, uint32_t length)
   else
   {
     // Loop over memory printing hex output
-    for(int i = 0; i < length; i++)
+    for(uint32_t i = 0; i < length; i++)
     {
       PRINTF("0x%02x ", *(start + i));
     }
     PRINTF("\n");
   }
-}
+} // print_memory()
