b6a276ab46e7e70e6412fdcb54f6e4d147383603
rmortens <ryan.j.mortenson@gmail.com>
2017-02-26 19:35:36 -0700

    main does retransmit using UART and circbuf

--- project/app/src/main.c
+++ project/app/src/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "circbuf.h"
 #include "project1.h"
 #include "log.h"
 #include "log_item.h"
@@ -6,6 +7,8 @@
 #include "uart.h"
 #endif // FRDM
 
+extern circbuf_t * receive;
+extern circbuf_t * transmit;
 
 int main()
 {
@@ -21,23 +24,37 @@ int main()
 #ifdef PROJECT2
   // Execute the required functions for project2
 #if FRDM
+
+  uint8_t byte = 0;
+
+  circbuf_init(&receive, 100);
+  circbuf_init(&transmit, 100);
+
   // Initialize uart
   uart_configure(BAUD_RATE);
-#endif // FRDM
 
   log_item_t * item;
   create_log_item(&item, LOG_ID_ERROR, (uint8_t *)"test", 5);
   log_item(item);
   destroy_log_item(item);
 
-  while(0)
+  while(1)
   {
-    LOG_RAW_STRING("TRYING LOG\n");
-    LOG_RAW_INT(-7235);
-    LOG_RAW_DATA("LOGGING DATA", 12);
+    if(circbuf_empty(receive) != CB_ENUM_NO_ERROR)
+    {
+      circbuf_remove_item(receive, &byte);
+      if (byte == 'q')
+      {
+        break;
+      }
+      circbuf_add_item(transmit, byte);
+      signal_transmit();
+    }
   }
 
+  circbuf_destroy(receive);
+  circbuf_destroy(transmit);
+#endif // FRDM
 #endif // PROJECT2
-
   return 0;
 }
