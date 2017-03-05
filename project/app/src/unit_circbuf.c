2eb6fa03e30efec6eefd09ef212941eaaa08f28c
rmortens <ryan.j.mortenson@gmail.com>
2017-03-05 11:08:10 -0700

    Cleanup and add comments to unit test files

--- project/app/src/unit_circbuf.c
+++ project/app/src/unit_circbuf.c
@@ -11,22 +11,23 @@
 #define HALF_BUF_SIZE (BUF_SIZE >> 1)
 
 // Result used in most tests
-uint32_t circbuf_res = 0;
 circbuf_t * buf = NULL;
 
 void test_circbuf_init_destroy(void **state)
 {
-  circbuf_res = circbuf_init(&buf, BUF_SIZE);
-  assert_int_equal(circbuf_res, CB_ENUM_NO_ERROR);
+  // Create circbuf and check there were no errors
+  assert_int_equal(circbuf_init(&buf, BUF_SIZE), CB_ENUM_NO_ERROR);
 
-  circbuf_res = circbuf_destroy(buf);
-  assert_int_equal(circbuf_res, CB_ENUM_NO_ERROR);
+  // Destroy circbuf and check there were no errors
+  assert_int_equal(circbuf_destroy(buf), CB_ENUM_NO_ERROR);
 } // test_circbuf_init_destroy()
 
 void test_circbuf_ops_null_ptr(void **state)
 {
   uint8_t value = 0;
 
+  // Pass a null pointer into each circbuf function and make sure they return
+  // null pointer enum
   assert_int_equal(circbuf_init((circbuf_t **)NULL, BUF_SIZE), CB_ENUM_NULL_POINTER);
   assert_int_equal(circbuf_destroy((circbuf_t *)NULL), CB_ENUM_NULL_POINTER);
   assert_int_equal(circbuf_add_item((circbuf_t *)NULL, 1), CB_ENUM_NULL_POINTER);
@@ -40,37 +41,42 @@ void test_circbuf_add_remove_full(void **state)
 {
   uint8_t value = 0;
 
-  circbuf_res = circbuf_init(&buf, BUF_SIZE);
-  assert_int_equal(circbuf_res, CB_ENUM_NO_ERROR);
+  // Create a circbuf and check there were no errors
+  assert_int_equal(circbuf_init(&buf, BUF_SIZE), CB_ENUM_NO_ERROR);
 
-  // Loop over buf size adding values
+  // Loop over buf size adding values checking there are no errors
   for (uint32_t i = 0; i < BUF_SIZE; i++)
   {
     assert_int_equal(circbuf_add_item(buf, i), CB_ENUM_NO_ERROR);
   }
 
-  // Loop over buf size removing values and checking contents
+  // Loop over buf size removing values and checking contents and checking
+  // there are no errors
   for (uint32_t i = 0; i < BUF_SIZE; i++)
   {
     assert_int_equal(circbuf_remove_item(buf, &value), CB_ENUM_NO_ERROR);
     assert_int_equal(value, i);
   }
 
-  circbuf_res = circbuf_destroy(buf);
-  assert_int_equal(circbuf_res, CB_ENUM_NO_ERROR);
+  // Destroy circbuf and check there were no errors
+  assert_int_equal(circbuf_destroy(buf), CB_ENUM_NO_ERROR);
 } // test_circbuf_add_remove_full()
 
 void test_circbuf_add_full(void **state)
 {
+  // Create circbuf and check there were no errors
   assert_int_equal(circbuf_init(&buf, BUF_SIZE), CB_ENUM_NO_ERROR);
 
-  // Loop over buf size adding values until full
+  // Loop over buf size adding values checking there are no errors
   for (uint32_t i = 0; i < BUF_SIZE; i++)
   {
     assert_int_equal(circbuf_add_item(buf, i), CB_ENUM_NO_ERROR);
   }
 
+  // Try to add an item when buffer is full to ensure it returns full
   assert_int_equal(circbuf_add_item(buf, 100), CB_ENUM_FULL);
+
+  // Destroy circbuf and check there were no errors
   assert_int_equal(circbuf_destroy(buf), CB_ENUM_NO_ERROR);
 } // test_circbuf_add__full()
 
@@ -78,8 +84,13 @@ void test_circbuf_remove_empty(void **state)
 {
   uint8_t value = 0;
 
+  // Create circbuf and check there were no errors
   assert_int_equal(circbuf_init(&buf, BUF_SIZE), CB_ENUM_NO_ERROR);
+
+  // Try to remove an item from the empty buffer and check return for empty
   assert_int_equal(circbuf_remove_item(buf, &value), CB_ENUM_EMPTY);
+
+  // Destroy circbuf and check there were no errors
   assert_int_equal(circbuf_destroy(buf), CB_ENUM_NO_ERROR);
 } // test_circbuf_add_remove_full()
 
@@ -87,58 +98,80 @@ void test_circbuf_wrap_add_remove(void **state)
 {
   uint8_t value = 0;
 
-  circbuf_res = circbuf_init(&buf, BUF_SIZE);
-  assert_int_equal(circbuf_res, CB_ENUM_NO_ERROR);
+  // Create circbuf and check there were no errors
+  assert_int_equal(circbuf_init(&buf, BUF_SIZE), CB_ENUM_NO_ERROR);
 
-  // Loop over buf size adding values
-  for (uint32_t i = 0; i < HALF_BUF_SIZE; i++) {
+  // Loop over half of buf size adding values and checking for no error
+  // This will place the head in the middle of the circular buffer
+  for (uint32_t i = 0; i < HALF_BUF_SIZE; i++)
+  {
     assert_int_equal(circbuf_add_item(buf, i), CB_ENUM_NO_ERROR);
   }
 
-  // Loop over buf size removing values and checking contents
+  // Loop over half of buf size removing values checking for no error and
+  // values are correct.
+  // This will place the tail in the middle of the circular buffer
   for (uint32_t i = 0; i < HALF_BUF_SIZE; i++)
   {
     assert_int_equal(circbuf_remove_item(buf, &value), CB_ENUM_NO_ERROR);
     assert_int_equal(value, i);
   }
 
-  // Loop over buf size adding values
+  // Loop over buf size adding values checking for no errors
+  // This wraps the head back to the beginning
   for (uint32_t i = 0; i < BUF_SIZE; i++)
   {
     assert_int_equal(circbuf_add_item(buf, i), CB_ENUM_NO_ERROR);
   }
 
-  // Loop over buf size removing values and checking contents
+  // Loop over buf size removing values checking contents and checking no errors
+  // This wraps the tail back to the beginning
   for (uint32_t i = 0; i < BUF_SIZE; i++)
   {
     assert_int_equal(circbuf_remove_item(buf, &value), CB_ENUM_NO_ERROR);
     assert_int_equal(value, i);
   }
 
-  circbuf_res = circbuf_destroy(buf);
-  assert_int_equal(circbuf_res, CB_ENUM_NO_ERROR);
+  // Destroy circbuf and check there were no errors
+  assert_int_equal(circbuf_destroy(buf), CB_ENUM_NO_ERROR);
 } // test_circbuf_wrap_add()
 
 void test_circbuf_check_full(void **state)
 {
+  // Create circbuf and check there were no errors
   assert_int_equal(circbuf_init(&buf, BUF_SIZE), CB_ENUM_NO_ERROR);
+
+  // Check to make sure the full function returns a failure
   assert_int_equal(circbuf_full(buf), CB_ENUM_FAILURE);
 
-  // Fill up buffer
+  // Loop over buf size adding values checking for no errors
   for (uint32_t i = 0; i < BUF_SIZE; i++)
   {
     assert_int_equal(circbuf_add_item(buf, i), CB_ENUM_NO_ERROR);
   }
 
+  // Check to make sure the full function returns full
   assert_int_equal(circbuf_full(buf), CB_ENUM_FULL);
+
+  // Destroy circbuf and check there were no errors
   assert_int_equal(circbuf_destroy(buf), CB_ENUM_NO_ERROR);
 } // test_circbuf_check_full()
 
 void test_circbuf_check_empty(void **state)
 {
+  // Create circbuf and check there were no errors
   assert_int_equal(circbuf_init(&buf, BUF_SIZE), CB_ENUM_NO_ERROR);
+
+  // Check for empty and make sure function returns empty because it is empty
   assert_int_equal(circbuf_empty(buf), CB_ENUM_EMPTY);
+
+  // Add one item to make buffer not empty
   assert_int_equal(circbuf_add_item(buf, 1), CB_ENUM_NO_ERROR);
+
+  // Check for empty and make sure function returns failure because it isn't
+  // empty
   assert_int_equal(circbuf_empty(buf), CB_ENUM_FAILURE);
+
+  // Destroy circbuf and check there were no errors
   assert_int_equal(circbuf_destroy(buf), CB_ENUM_NO_ERROR);
 } // test_circbuf_check_full()
--- project/app/src/unit_memory.c
+++ project/app/src/unit_memory.c
@@ -16,107 +16,125 @@ uint8_t *str = (uint8_t *)"1234567890";
 // Buffer user for memory tests
 uint8_t src[STR_LEN + 1] = {0};
 
-// Result used in most tests
-uint32_t memory_res = 0;
-
 // Test memmove normally
 void test_my_memmove(void **state)
 {
-  memory_res = my_memmove(str, src, STR_LEN);
-  assert_int_equal(memory_res, SUCCESS);
+  // Do a normal move from str to src checking for success
+  assert_int_equal(my_memmove(str, src, STR_LEN), SUCCESS);
+
+  // Validate memory should be equal
   assert_memory_equal((void *)src, (void *)str, STR_LEN);
 } // test_my_memmove()
 
 // Test memmove where destination starts in source
 void test_my_memmove_dst_starts_in_src(void **state)
 {
-  memory_res = my_memmove(str, src, STR_LEN);
-  memory_res = my_memmove((src + 2), (src + 5), 4);
-  assert_int_equal(memory_res, SUCCESS);
+  // Do a normal move from str to src checking for success
+  assert_int_equal(my_memmove(str, src, STR_LEN), SUCCESS);
+
+  // Do a a move where the dst starts in src
+  assert_int_equal(my_memmove((src + 2), (src + 5), 4), SUCCESS);
+
+  // Validate memory should be equal to string
   assert_memory_equal((void *)src, (void *)"1234534560", STR_LEN);
 } // test_my_memmove_dst_starts_in_src()
 
 // Test memmove where source starts in destination
 void test_my_memmove_dst_ends_in_src(void **state)
 {
-  memory_res = my_memmove(str, src, STR_LEN);
-  memory_res = my_memmove((src + 5), (src + 1), 4);
-  assert_int_equal(memory_res, SUCCESS);
+  // Do a normal move from str to src checking for success
+  assert_int_equal(my_memmove(str, src, STR_LEN), SUCCESS);
+
+  // Do a a move where the src starts in dst
+  assert_int_equal(my_memmove((src + 5), (src + 1), 4), SUCCESS);
+
+  // Validate memory should be equal to string
   assert_memory_equal((void *)src, (void *)"1678967890", STR_LEN);
 } // test_my_memmove_dst_ends_in_src()
 
 // Test memmove where null pointers are passed in for source and destination
 void test_my_memmove_null_ptrs(void **state)
 {
-  memory_res = my_memmove((uint8_t *)NULL, src, 4);
-  assert_int_equal(memory_res, NULL_POINTER);
-  memory_res = my_memmove(src, (uint8_t *)NULL, 4);
-  assert_int_equal(memory_res, NULL_POINTER);
+  // Pass a null pointer into src and ensure it returns null pointer
+  assert_int_equal(my_memmove((uint8_t *)NULL, src, 4), NULL_POINTER);
+
+  // Pass a null pointer into dst and ensure it returns null pointer
+  assert_int_equal(my_memmove(src, (uint8_t *)NULL, 4), NULL_POINTER);
 } // test_my_memmove_null_ptrs()
 
 // Test memset normally
 void test_my_memset(void **state)
 {
-  memory_res = my_memset(src, STR_LEN, 'A');
-  assert_int_equal(memory_res, SUCCESS);
+  // Do a normal memset and ensure memory was set properly
+  assert_int_equal(my_memset(src, STR_LEN, 'A'), SUCCESS);
   assert_memory_equal((void *)src, (void *)"AAAAAAAAAA", STR_LEN);
 } // test_my_memset()
 
 // Test memset where a null pointer is passed in for source
 void test_my_memset_null_ptr(void **state)
 {
-  memory_res = my_memset((uint8_t *)NULL, STR_LEN, 'A');
-  assert_int_equal(memory_res, NULL_POINTER);
+  // Pass a null pointer into src and ensure it returns null pointer
+  assert_int_equal(my_memset((uint8_t *)NULL, STR_LEN, 'A'), NULL_POINTER);
 } // test_my_memset_null_ptr()
 
 // Test memzero normally
 void test_my_memzero(void **state)
 {
   uint8_t zeros[10] = {0};
-  memory_res = my_memzero(src, STR_LEN);
-  assert_int_equal(memory_res, SUCCESS);
+
+  // Do a normal memmzero and ensure memory is zeroed out
+  assert_int_equal(my_memzero(src, STR_LEN), SUCCESS);
   assert_memory_equal((void *)src, (void *)zeros, STR_LEN);
 } // test_my_memzero()
 
 // Test memzero where a null pointer is passed in for source
 void test_my_memzero_null_ptr(void **state)
 {
-  memory_res = my_memzero((uint8_t *)NULL, STR_LEN);
-  assert_int_equal(memory_res, NULL_POINTER);
+  // Pass a null pointer into src and ensure it returns null pointer
+  assert_int_equal(my_memzero((uint8_t *)NULL, STR_LEN), NULL_POINTER);
 } // test_my_memzero_null_ptr()
 
 // Test reverse on an odd string
 void test_my_reverse_odd(void **state)
 {
-  memory_res = my_memmove(str, src, STR_LEN);
-  memory_res = my_reverse(src, STR_LEN - 1);
-  assert_int_equal(memory_res, SUCCESS);
+  // Do a normal move from str to src checking for success
+  assert_int_equal(my_memmove(str, src, STR_LEN), SUCCESS);
+
+  // Do a reverse on an odd length string and ensure memory is
+  // equal to reversed string
+  assert_int_equal(my_reverse(src, STR_LEN - 1), SUCCESS);
   assert_memory_equal((void *)src, (void *)"987654321", STR_LEN - 1);
 } // test_my_reverse_odd()
 
 // Test reverse on an even string
 void test_my_reverse_even(void **state)
 {
-  memory_res = my_memmove(str, src, STR_LEN);
-  memory_res = my_reverse(src, STR_LEN);
-  assert_int_equal(memory_res, SUCCESS);
+  // Do a normal move from str to src checking for success
+  assert_int_equal(my_memmove(str, src, STR_LEN), SUCCESS);
+
+  // Do a reverse on an even length string and ensure memory is
+  // equal to reversed string
+  assert_int_equal(my_reverse(src, STR_LEN), SUCCESS);
   assert_memory_equal((void *)src, (void *)"0987654321", STR_LEN);
 } // test_my_reverse_even()
 
 // Test reverse on single character
 void test_my_reverse_single(void **state)
 {
-  memory_res = my_memmove((uint8_t *)"a", src, 1);
-  memory_res = my_reverse(src, 1);
-  assert_int_equal(memory_res, SUCCESS);
+  // Put a 1 character string in src and ensure success
+  assert_int_equal(my_memmove((uint8_t *)"a", src, 1), SUCCESS);
+
+  // Do a reverse on a one character string and ensure memory is
+  // equal to reversed string
+  assert_int_equal(my_reverse(src, 1), SUCCESS);
   assert_memory_equal((void *)src, (void *)"a", 1);
 } // test_my_reverse_single()
 
 // Test reverse where a null pointer is passed in for source
 void test_my_reverse_null_ptr(void **state)
 {
-  memory_res = my_reverse((uint8_t *)NULL, STR_LEN);
-  assert_int_equal(memory_res, NULL_POINTER);
+  // Pass a null pointer into src and ensure it returns null pointer
+  assert_int_equal(my_reverse((uint8_t *)NULL, STR_LEN), NULL_POINTER);
 } // test_my_reverse_null_prt()
 
 // Test reverse with a 255 byte string
@@ -126,13 +144,14 @@ void test_my_reverse_max(void **state)
   uint8_t buffer[MAX];
   uint8_t rev_buffer[MAX];
 
+  // Create a max length buffer and reverse max length buffer
   for (int i = 0; i < MAX; i++)
   {
     buffer[i] = i;
     rev_buffer[i] = MAX - i - 1;
   }
 
-  memory_res = my_reverse(buffer, MAX);
-  assert_int_equal(memory_res, SUCCESS);
+  // Reverse string ensuring success and check result is reversed
+  assert_int_equal(my_reverse(buffer, MAX), SUCCESS);
   assert_memory_equal(rev_buffer, buffer, MAX);
 } // test_my_reverse_max()
