1868c7d818905b90d2198363893beb4281982316
rmortens <ryan.j.mortenson@gmail.com>
2017-03-29 06:40:01 -0600

    Add comments to rtc.c

--- project/prop/kl25z/src/rtc.c
+++ project/prop/kl25z/src/rtc.c
@@ -12,6 +12,12 @@
   static log_item_t * item;
 #endif // VERBOSE
 
+// Used to set pins to GPIO setting
+#define ALT_1 (1)
+#define ALT_5 (5)
+#define RTC_CLKIN (2)
+#define MCGIRCLK (4)
+
 extern void RTC_Seconds_IRQHandler()
 {
   CREATE_ITEM_STRING(item, LOG_ID_HEARTBEAT, "HEARTBEAT");
@@ -22,12 +28,25 @@ void rtc_init()
 {
   // Turn on internal reference clock
   MCG_C1 |= MCG_C1_IRCLKEN_MASK;
+
+  // Select slow internal reference clock
   MCG_C2 &= ~(MCG_C2_IRCS_MASK);
+
+  // Allow clocks to go to port c
   SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC_PCR1 |= (PORT_PCR_MUX(0x1));
-  SIM_SOPT1 |= SIM_SOPT1_OSC32KSEL(0b10);
-  SIM_SOPT2 |= SIM_SOPT2_CLKOUTSEL(0b100);
-  PORTC_PCR3 |= (PORT_PCR_MUX(0x5));
+
+  // Set pin C1 to alt 1 for gpio
+  PORTC_PCR1 |= (PORT_PCR_MUX(ALT_1));
+
+  // Select RTC clock in for 32kHz oscillator
+  SIM_SOPT1 |= SIM_SOPT1_OSC32KSEL(RTC_CLKIN);
+
+  // Select the clock out on pin C5 as internal reference clock
+  SIM_SOPT2 |= SIM_SOPT2_CLKOUTSEL(MCGIRCLK);
+
+  // Set pin C5 to clock out
+  PORTC_PCR3 |= (PORT_PCR_MUX(ALT_5));
+
   // Turn on access and interrupts
   SIM_SCGC6 |= SIM_SCGC6_RTC_MASK;
 
@@ -40,6 +59,9 @@ void rtc_init()
 
   // Enable the IRQ for the RTC seconds
   NVIC_EnableIRQ(RTC_Seconds_IRQn);
+
+  // Set the priority lower than UART to allow interruption by UART
+  // for printing the heartbeat
   NVIC_SetPriority(RTC_Seconds_IRQn, 10);
 
   // Clear all interrupts then set the timer seconds interrupt
